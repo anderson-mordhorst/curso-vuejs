666108b491bf3acac249154a4c865fc1c62a16e0
anderson-mordhorst <anderson.mordhorst@gmail.com>
2021-10-20 20:40:07 -0300

    Exercício de diretivas

---
+++ diretivas/diretivas-exercicios/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+Vue.directive('destaque', {
+	// el, binding, vnode
+	bind(el, binding) {
+		//el.style.backgroundColor = 'lightgreen';
+		if(binding.arg === 'fundo') {
+			el.style.backgroundColor = binding.value;
+		} else {
+			el.style.color = binding.value;
+		}
+	}
+});
+
+new Vue({
+	render: h => h(App),
+}).$mount('#app')
