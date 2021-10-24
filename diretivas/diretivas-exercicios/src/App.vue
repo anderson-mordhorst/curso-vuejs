86b6cb694f63ff191cc5e060eced0b0eeb21f0a4
anderson-mordhorst <anderson.mordhorst@gmail.com>
2021-10-24 17:56:11 -0300

    Exercício de diretivas com modificadores

--- diretivas/diretivas-exercicios/src/App.vue
+++ diretivas/diretivas-exercicios/src/App.vue
@@ -2,15 +2,45 @@
 	<div id="app">
 		<h1>Diretivas</h1>
 		<hr>
-		<p v-destaque:fundo="'yellow'">Usando diretiva personalizada</p>
-		<p v-destaque="'green'">Usando diretiva personalizada sem fundo</p>
+		<p v-destaque-local:fundo.atrasar="'yellow'">Usando diretiva personalizada</p>
+		<p v-destaque-local.atrasar.alternar="'green'">Usando diretiva personalizada sem fundo</p>
 		<!-- <v-teste:argumento.modificador.modificador="'valor'"/>	-->
 	</div>
 </template>
 
 <script>
 export default {
-	
+	directives: {
+		'destaque-local': {
+			bind(el, binding) {
+				const aplicarCor = cor => {
+					if(binding.arg === 'fundo') {
+						el.style.backgroundColor = cor;
+					} else {
+						el.style.color = cor;
+					}
+				}
+
+				// 3 segundos de atraso
+				const atraso = binding.modifiers['atrasar'] ? 3000 : 0;
+
+				const cor1 = binding.value;
+				const cor2 = 'purple';
+				let corAtual = cor1;
+
+				setTimeout(() => {
+					if(binding.modifiers['alternar']) {
+						setInterval(() => {
+							corAtual = corAtual === cor1 ? cor2 : cor1;
+							aplicarCor(corAtual);
+						}, 1000)
+					} else {
+						aplicarCor(binding.value);
+					}					
+				}, atraso);
+			}			
+		}
+	},
 }
 </script>
 
--- diretivas/diretivas-exercicios/src/main.js
+++ diretivas/diretivas-exercicios/src/main.js
@@ -6,12 +6,17 @@ Vue.config.productionTip = false
 Vue.directive('destaque', {
 	// el, binding, vnode
 	bind(el, binding) {
-		//el.style.backgroundColor = 'lightgreen';
-		if(binding.arg === 'fundo') {
-			el.style.backgroundColor = binding.value;
-		} else {
-			el.style.color = binding.value;
-		}
+		// 3 segundos de atraso
+		const atraso = binding.modifiers['atrasar'] ? 3000 : 0;
+
+		setTimeout(() => {
+			//el.style.backgroundColor = 'lightgreen';
+			if(binding.arg === 'fundo') {
+				el.style.backgroundColor = binding.value;
+			} else {
+				el.style.color = binding.value;
+			}
+		}, atraso);
 	}
 });
 
