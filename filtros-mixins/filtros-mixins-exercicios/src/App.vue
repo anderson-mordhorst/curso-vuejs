da3ca16fa36491c3a281c4cf455261953812af9d
anderson-mordhorst <anderson.mordhorst@gmail.com>
2021-10-26 20:13:37 -0300

    Exercício de filtros e mixins

---
+++ filtros-mixins/filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+		<hr>
+		<p>{{ cpf | cpfFilter | inverter }}</p>
+		<input type="text" :value="cpf | cpfFilter">
+		<!-- | cpfFilter aplica o filtro -->	
+		<hr>
+		<frutas/>
+		<hr>
+	</div>
+</template>
+
+<script>
+import Frutas from './Frutas.vue';
+import frutasMixin from './frutasMixin';
+
+export default {
+  components: { Frutas },
+  mixins: [ frutasMixin ],
+	filters: {
+		cpfFilter(valor){
+			const array = valor.split('');
+			array.splice(3, 0, '.');
+			array.splice(7, 0, '.');
+			array.splice(11, 0, '-');
+			return array.join('');
+		},
+	},
+	data() {
+		return {
+			cpf: '60070080090',
+		};
+	},
+}
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+		font-size: 2.5rem;
+	}
+
+	input {
+		font-size: 2.5rem;
+	}
+</style>
---
+++ filtros-mixins/filtros-mixins-exercicios/src/Frutas.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(currentFruta, index) in frutas" :key="index"> {{ currentFruta }}</li>
+        </ul>
+        <input type="text" v-model="fruta" @keydown.enter="add"/>
+    </div>
+</template>
+
+<script>
+import frutasMixin from './frutasMixin';
+
+export default {
+    mixins: [ frutasMixin ],
+}
+</script>
+
+<style>
+
+</style>
