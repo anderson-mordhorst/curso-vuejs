9a5cc9226fe99fd673098eb58dd56436a4a9cf0c
anderson-mordhorst <anderson.mordhorst@gmail.com>
2021-11-22 19:54:31 -0300

    Exercício de rotas com vue-router

---
+++ rotas/rotas-exercicios/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="app">
+		<h1>Rotas com VueRouter</h1>
+		<Menu/>
+		<!-- local onde será carregado os componentes da rota -->
+		<router-view />
+	</div>
+</template>
+
+<script>
+import Menu from '@/components/template/Menu.vue';
+
+export default {
+	components: { Menu },
+}
+</script>
+
+<style>
+	#app {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
---
+++ rotas/rotas-exercicios/src/components/template/Menu.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="menu">
+        <ul>
+            <!-- Sem o exact, caso seja navegado para o usuário o link fica ativo porque /usuario possui o / -->
+            <router-link to="/" tag="li" active-class="active" exact><a>Início</a></router-link>
+            <router-link to="/usuario" tag="li" active-class="active"><a>Usuário</a></router-link>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
