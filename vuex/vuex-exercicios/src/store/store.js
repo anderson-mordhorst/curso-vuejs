cfdd8da1a220017d5f17696112dcff73c9499e4b
anderson-mordhorst <anderson.mordhorst@gmail.com>
2021-11-25 20:40:14 -0300

    Exercício de vuex: getters e mapGetters

--- vuex/vuex-exercicios/src/store/store.js
+++ vuex/vuex-exercicios/src/store/store.js
@@ -6,5 +6,12 @@ Vue.use(Vuex);
 export default new Vuex.Store({
     state: {
         produtos: [],
-    }, 
+    },
+    getters: {
+        // retorna o cálculo do valor total dos produtos
+        valorTotal(state) {
+            return state.produtos.map(p => p.quantidade * p.preco)
+                .reduce((total, atual) => total + atual, 0)
+        },
+    },
 });
