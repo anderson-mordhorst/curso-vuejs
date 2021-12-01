551521592b889ffe092506b16cb610087f3f123d
anderson-mordhorst <anderson.mordhorst@gmail.com>
2021-12-01 19:22:42 -0300

    Projeto 03: adicionado vuex e vue-router

--- projeto-03-acoes/stock-trader/src/main.js
+++ projeto-03-acoes/stock-trader/src/main.js
@@ -1,9 +1,13 @@
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
+import Vue from 'vue';
+import './plugins/vuetify';
+import App from './App.vue';
+import router from '@/router.js';
+import store from '@/store.js';
 
 Vue.config.productionTip = false
 
 new Vue({
+	router,
+	store,
 	render: h => h(App),
 }).$mount('#app')
