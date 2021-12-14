7731b3af9486ce040c9905d5b87b51a80c5ca472
anderson-mordhorst <anderson.mordhorst@gmail.com>
2021-12-14 19:13:32 -0300

    Projeto 04: calculadora

---
+++ projeto-04-calculadora/calculator/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <h1>Olá Vue</h1>
+  </div>
+</template>
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+  * {
+    font-family: 'monospace';
+  }
+
+  body {
+    margin: 0;
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: white;
+  }  
+</style>
