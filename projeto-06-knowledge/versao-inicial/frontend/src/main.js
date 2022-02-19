a76e404bf62bc1160ffbe7c4b771286c92df9908
anderson-mordhorst <anderson.mordhorst@gmail.com>
2022-02-19 09:05:01 -0300

    Projeto 06: dashboard

--- projeto-06-knowledge/versao-inicial/frontend/src/main.js
+++ projeto-06-knowledge/versao-inicial/frontend/src/main.js
@@ -7,6 +7,9 @@ import router from '@/config/router';
 
 Vue.config.productionTip = false;
 
+// TEMPORARIO
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFuZGVyc29uIE1vcmRob3JzdCIsImVtYWlsIjoiYW5kZXJzb24ubW9yZGhvcnN0QGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NDUyNzE1MDEsImV4cCI6MTY0NTUzMDcwMX0.MmWIX9q13GMiYmUvlUpJB8dTlHRJv-1hWaqnC42-TzU';
+
 new Vue({
     store,
     router,
